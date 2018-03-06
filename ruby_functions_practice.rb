090b595eb4f8ab558940df321024633d7ab92ef5
Jaime Lopez <detec.azul@gmail.com>
2018-03-06 16:11:22 +0000

    First commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,102 @@
+def return_10()
+  return 10
+end
+
+def add(firstnumber, secondnumber)
+  return firstnumber + secondnumber
+end
+
+def subtract(firstnumber, secondnumber)
+  return firstnumber - secondnumber
+end
+
+def multiply(firstnumber, secondnumber)
+  return firstnumber * secondnumber
+end
+
+def divide(firstnumber, secondnumber)
+  return firstnumber / secondnumber
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(firststring, secondstring)
+  return firststring + secondstring
+end
+
+def add_string_as_number(firststring, secondstring)
+  return firststring.to_i + secondstring.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  end
+
+end
+
+def number_to_short_month_name(number)
+  case number
+  when 1
+    return "Jan"
+  when 2
+    return "Feb"
+  when 3
+    return "Mar"
+  when 4
+    return "Apr"
+  when 5
+    return "May"
+  when 6
+    return "Jun"
+  when 7
+    return "Jul"
+  when 8
+    return "Aug"
+  when 9
+    return "Sep"
+  when 10
+    return "Oct"
+  when 11
+    return "Nov"
+  when 12
+    return "Dec"
+  end
+end
+
+def volume_of_cube(side)
+  return side ** 3
+end
+
+def volume_of_sphere(radius)
+  ((4/3)*Math::PI*radius**3).round(2)
+end
+
+def fahrenheit_to_celsius(fahrenheit)
+  return fahrenheit - 17.22
+end 
