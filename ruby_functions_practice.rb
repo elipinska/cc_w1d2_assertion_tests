33804c848126a0b00389709ebfd32f12cf7f4c28
elipinska <ewa.lipinska@hotmail.com>
2018-03-07 10:16:39 +0000

    Simplify number_to_short_month_name function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -36,8 +36,8 @@ def number_to_full_month_name(number)
 end
 
 def number_to_short_month_name(number)
-  months = [nil, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-  return months[number]
+  full_month = number_to_full_month_name(number)
+  return full_month.slice(0,3)
 end
 
 def volume_of_cube(side)
